f01360bf2bdceb1b65c08c75ca914ab2544eeffb
Eriton Xavier <ertao1@hotmail.com>
2022-02-02 16:43:29 -0300

    H2 banco de dados, perfil de teste, JPA

--- src/main/java/com/eriton/curso/entities/User.java
+++ src/main/java/com/eriton/curso/entities/User.java
@@ -3,9 +3,17 @@ package com.eriton.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -66,7 +74,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
