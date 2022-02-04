1159d105d04c02ab64fc97ddc2365b84e17b6e7b
Eriton Xavier <ertao1@hotmail.com>
2022-02-04 16:02:24 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/eriton/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.eriton.curso.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.eriton.curso.services.exceptions.ResourceNotFoundException;
+
+
+@ControllerAdvice   //Conselho do Controlador
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)   // Manipulador de exceção
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
