184ac2f563cad065b03088049fa470becb86284c
Eriton Xavier <ertao1@hotmail.com>
2022-02-04 09:39:12 -0300

    Pagamento, associação um para um

--- src/main/java/com/eriton/curso/config/TestConfig.java
+++ src/main/java/com/eriton/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.eriton.curso.entities.Category;
 import com.eriton.curso.entities.Order;
 import com.eriton.curso.entities.OrderItem;
+import com.eriton.curso.entities.Payment;
 import com.eriton.curso.entities.Product;
 import com.eriton.curso.entities.User;
 import com.eriton.curso.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 		
 	}
 }
